d1658693d16a15d0ef7e6a0c3c089e7f89e74f94
unknown <houssam.ouederni@gmail.com>
2022-02-05 15:32:46 +0100

    lo

--- tp1/lib/media.dart
+++
@@ -1,9 +0,0 @@
-class Media{
-  final String titre;
-  final String desc;
-  final String annee;
-  final String image;
-  final double rating;
-
-  const Media(this.titre, this.desc, this.annee, this.image, this.rating);
-}
